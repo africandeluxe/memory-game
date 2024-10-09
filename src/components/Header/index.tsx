da809d372a9147606780bbdc64af4eaee31064e1
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-10-09 23:14:18 +0200

    fixed logo error

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from '../public/logo.png'; 
 
 const Header: React.FC = () => {
   return (
     <header className="bg-yellow-400 p-5 text-center shadow-md">
-      <img src={logo} alt="logo" className="mx-auto w-16 h-16 mb-2" role="img" aria-label="logo" />
+      <img src="/logo.png" alt="logo" className="mx-auto w-16 h-16 mb-2" role="img" aria-label="logo" />
       <h1 className="text-pink-500 text-4xl font-bold">Memorista</h1>
     </header>
   );
