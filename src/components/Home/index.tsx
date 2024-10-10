e3ebd568842477fe44f8087975c2f4063bb474e9
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-10-10 22:56:08 +0200

    minor changes

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 import NewGameButton from '../NewGameBtn';
 import Card from '../Card';
 
-// Function to generate a deck of cards
+
 const generateDeck = () => {
   const cards: string[] = [
     "/guitar.svg",
@@ -14,10 +14,9 @@ const generateDeck = () => {
     "/flower.svg",
     "/cabin.svg",
   ];
-  return [...cards, ...cards]; // Duplicate the cards to create pairs
+  return [...cards, ...cards];
 };
 
-// Define CardType interface
 interface CardType {
   id: number;
   image: string;
@@ -26,7 +25,7 @@ interface CardType {
 }
 
 const Home: React.FC = () => {
-  // Generate initial cards using the imported SVG images
+
   const initialCards: CardType[] = generateDeck().map((image, index) => ({
     id: index,
     image: image,
@@ -34,7 +33,6 @@ const Home: React.FC = () => {
     isMatched: false,
   }));
 
-  // Set state for cards, moves, highscore, and flipped cards
   const [cards, setCards] = useState<CardType[]>(shuffleCards(initialCards));
   const [moves, setMoves] = useState<number>(0);
   const [highscore, setHighscore] = useState<number>(() => {
@@ -43,12 +41,10 @@ const Home: React.FC = () => {
   });
   const [flippedCards, setFlippedCards] = useState<number[]>([]);
 
-  // Function to shuffle cards
   function shuffleCards(cards: CardType[]): CardType[] {
     return [...cards].sort(() => Math.random() - 0.5);
   }
 
-  // Handle card click
   const handleCardClick = (index: number) => {
     if (flippedCards.length === 2 || cards[index].isFlipped || cards[index].isMatched) {
       return;
@@ -61,7 +57,6 @@ const Home: React.FC = () => {
     setMoves((prev) => prev + 1);
   };
 
-  // Effect to check for matching cards when two cards are flipped
   useEffect(() => {
     if (flippedCards.length === 2) {
       const [firstIndex, secondIndex] = flippedCards;
@@ -86,14 +81,12 @@ const Home: React.FC = () => {
     }
   }, [flippedCards, cards]);
 
-  // Handle starting a new game
   const handleNewGame = () => {
     setCards(shuffleCards(initialCards));
     setMoves(0);
     setFlippedCards([]);
   };
 
-  // Effect to update highscore when all cards are matched
   useEffect(() => {
     if (cards.every((card) => card.isMatched)) {
       if (moves < highscore) {
@@ -109,15 +102,10 @@ const Home: React.FC = () => {
       <h3 data-testid="highscore" className="text-xl mt-2">Highscore: {highscore === Infinity ? "N/A" : highscore}</h3>
       <div className="grid grid-cols-4 gap-5 mt-8">
         {cards.map((card, index) => (
-          <Card
-            key={card.id}
-            flipped={card.isFlipped}
-            matched={card.isMatched}
-            imageSrc={card.image}
-            onClick={() => handleCardClick(index)}
-          />
+          <Card key={card.id} flipped={card.isFlipped} matched={card.isMatched} imageSrc={card.image} onClick={() => handleCardClick(index)} />
         ))}
       </div>
+      
       <NewGameButton newRound={handleNewGame} data-testid="new-game-btn" />
     </div>
   );
